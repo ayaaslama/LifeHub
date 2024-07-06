7e6899da6a56c94b49ce7a63c3d545903e5b8356
ayaaslama <ayamhmd089@gmail.com>
2024-07-06 12:01:09 +0300

    splash_screen

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:blood_life/core/theaming/color.dart';
 import 'package:blood_life/core/theaming/fonts_weight_helper.dart';
-import 'package:blood_life/features/questionnaires/ui/questionnaires.dart';
+import 'package:blood_life/features/onboarding/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (_) => const Questionnaires()));
+          context, MaterialPageRoute(builder: (_) => const OnBoardingScreen()));
     });
     super.initState();
   }
